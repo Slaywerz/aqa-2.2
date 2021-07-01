7764415543aa8080cf5dea3ab9793b5818c6902a
Rustem Mirzakaev <rustem.mirzakaev@gmail.com>
2021-07-02 00:48:13 +0500

    Project added

---
+++ src/test/java/ru/netology/rest/PostmanEcho.java
@@ -0,0 +1,33 @@
+package ru.netology.rest;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+
+public class PostmanEcho {
+
+    @BeforeAll
+    public static void setup() {
+        RestAssured.baseURI = "https://postman-echo.com";
+    }
+
+    @Test
+    void shouldReturnPostmanEcho() {
+        given()
+                .header("Content-type", "application/json")
+                .and()
+                .body("some data")
+                //Выполняемые действия
+                .when()
+                .post("/post")
+                //Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+                .body("headers.host", equalTo("postman-echo.crom"));
+    }
+}
